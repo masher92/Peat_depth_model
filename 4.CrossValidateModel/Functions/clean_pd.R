66cafe74898cc4000dba20331158ae0491c6d415
masher92 <43998529+masher92@users.noreply.github.com>
2020-10-27 12:07:31 +0100

    Update clean_pd.R

--- 4.CrossValidateModel/Functions/clean_pd.R
+++ 4.CrossValidateModel/Functions/clean_pd.R
@@ -1,43 +1,57 @@
-convertToSPDF <- function  (datafile){
-  # Convert back to shapefile
-  shp <- SpatialPointsDataFrame(coords = datafile[c(4:5)], data = datafile[c(1,3,7,8)],
-                                proj4string = CRS("+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +datum=OSGB36 +units=m +no_defs
-                                                  +ellps=airy +towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894"))
-  return (shp)
-}
-
-# Function which:
-#   Checks for the presence of coordinates with duplicated X and Y coordinates
-#   Removes the duplicates
-# It takes as input:
-#   1. a spatial points data frame
-find_duplication <- function (shp) {
+find_duplication <- function (spdf) {
+ "
+Description
+----------
+    Checks through a spatial points dataframe and finds any rows of data with duplicated X,Y coordinates, and removes these rows
+Parameters
+----------
+    spdf: Spatial points data frame (spdf)
+        A spdf containing the locations of points and related data
+Returns
+-------
+    spdf: Spatial points data frame (spdf)
+        A spdf containing the locations of points and related data, with any rows with duplicated lat/lons values removed
+"  
+   
   # Convert to a shapefile
   print ("Table detailing the presence of duplicates in this dataset")
-  print(data.frame(table(duplicated(shp@coords[,c(1:2)]))))
+  print(data.frame(table(duplicated(spdf@coords[,c(1:2)]))))
   # Delete the duplicates
-  shp <- remove.duplicates(shp)
+  spdf <- remove.duplicates(spdf)
   # test they have been removed
   print ("Table detailing the successful removal of duplicates from the dataset")
-  print(data.frame(table(duplicated(shp@coords[,c(1:2)]))))
-  return (shp)
+  print(data.frame(table(duplicated(spdf@coords[,c(1:2)]))))
+  return (spdf)
 }
 
-# Function which:
-#   Finds the distance between each x/y coordinate and its nearest neighbour.
-#   Deletes one from any pair which are more than 1m close to each other.
-# It takes as inputs:
-# 1.
-find_nearestNeighours <- function (shp, projection) {
-  # Convert to a dataframe
-  dat <- as.data.frame(shp)
+find_nearestNeighours <- function (spdf, projection) {
+"
+Description
+----------
+    Checks through a spatial points dataframe and finds the distance between the location of each point and its nearest neighbour.
+    Deletes one from any pair which are more than 1m close to each other.
+Parameters
+----------
+    spdf: Spatial points data frame (spdf)
+        A spdf containing the locations of points and related data
+    projection: string
+        A string specifyingt the projection, either 'bng' or 'wgs84
+Returns
+-------
+    new_spdf: Spatial points data frame (spdf)
+        A spdf containing the locations of points and related data, with one from each of any pair of locations which
+        are <1m apart removed.
+"  
+  
+  # Convert the spdf to a dataframe
+  dat <- as.data.frame(spdf)
   # Rename columns
   colnames(dat)[c(4,5)] <- c('longitude', 'latitude')
   # Calculate pairwise distances between points
   if (projection == 'bng'){
-    d <- gDistance(shp, byid=T) # rgeos package
+    d <- gDistance(spdf, byid=T) # rgeos package
   }else if(projection == 'wgs84') {
-    d <- distm(shp)}
+    d <- distm(spdf)}
   
   # Find second shortest distance (closest distance is of point to itself, therefore use second shortest)
   # For each row (which represents one sample point) place in an ascending order and select the 2nd point)
@@ -58,37 +72,10 @@ find_nearestNeighours <- function (shp, projection) {
   print(paste0("This sample contained ",nrow(less_than_1), " samples which are <1m apart. Now, one from each neighbouring pair has been removed"))
   
   # Convert back to shapefile
-  original_crs = crs(shp)
-  new_shp <- SpatialPointsDataFrame(coords = dat[c('longitude', 'latitude')], data = dat[c('depth', 'elevation', 'Slope_5m')],
+  original_crs = crs(spdf)
+  new_spdf <- SpatialPointsDataFrame(coords = dat[c('longitude', 'latitude')], data = dat[c('depth', 'elevation', 'Slope_5m')],
                                 proj4string = CRS(original_crs@projargs))
   
-  return (new_shp)
+  return (new_spdf)
 }
-
-
-# Function which:
-#   Finds the distance between each x/y coordinate and its nearest neighbour.
-#   Deletes one from any pair which are more than 1m close to each other.
-# It takes as inputs:
-convert_projection <- function (input, projection_system_required) {
-  # If a dataframe 
-  if(projection_system_required == 'wgs84'){
-    projection_system = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'}
-  if (projection_system_required == 'bng'){
-    projection_system = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs towgs84='446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894'"
-  } 
-  converted = spTransform(input,CRS(projection_system))
-  
-  # If a dataframe 
-  #if(is.data.frame(dtm)){
-  #  print("Its a dataframe")
-  #  
-  #} else {
-  #  print("Its not a dataframe")
-  #  # Convert projection
-  #  converted = spTransform(input,CRS(projection_system_required))
-  #}
-  return (converted)}
-
-
 
