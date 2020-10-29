db224b41299322dbf1920f39951923c9f64bef66
Molly Asher <m.asher@hotmail.com>
2020-10-29 10:32:37 +0000

    Moving working directory into config file

---
+++ config.R
@@ -0,0 +1,4 @@
+############################
+# Define working directory where all scripts and data are stored
+###########################
+working_directory = "C:/Users/gy17m2a/OneDrive - University of Leeds/Msc/Dissertation/DataAnalysis"
