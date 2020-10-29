ca74b49edd85cb6a7408972026bd173cefa31b3c
masher92 <43998529+masher92@users.noreply.github.com>
2020-10-29 11:41:29 +0100

    Delete config.R

--- config.R
+++
@@ -1,4 +0,0 @@
-############################
-# Define working directory where all scripts and data are stored
-###########################
-working_directory = "C:/Users/gy17m2a/OneDrive - University of Leeds/Msc/Dissertation/DataAnalysis"
